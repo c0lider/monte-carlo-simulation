a3c04552458201ae83f45191a7c6875354f42f69
olli <olli.seiffermann@outlook.de>
2019-01-15 14:58:20 +0100

    removed border around canvas

--- visible_canvas.py
+++ visible_canvas.py
@@ -6,11 +6,10 @@ class VisibleCanvas(Canvas):
     RECTANGLE_COLOR = "#FFFFFF"
     BG_COLOR = "#161618"
 
-    # TODO make the size of the canvas changeable
     def __init__(self, window):
-        super().__init__(window, width=420, height=420, bg=self.BG_COLOR)
-        self.rectangle = self.create_rectangle(10, 10, 410, 410, fill=self.RECTANGLE_COLOR, width=0)
-        self.circle = self.create_oval(10, 10, 410, 410, fill=self.CIRCLE_COLOR, width=0)
+        super().__init__(window, width=420, height=420, borderwidth=0)
+        self.rectangle = self.create_rectangle(0, 0, 420, 420, fill=self.RECTANGLE_COLOR, width=0)
+        self.circle = self.create_oval(0, 0, 420, 420, fill=self.CIRCLE_COLOR, width=0)
 
     def draw_point(self, x, y, color):
-        self.create_oval(x - 1, y - 1, x + 1, y + 1, width=0, fill=color)
+        self.create_oval(x, y, x + 1, y + 1, width=0, fill=color)
