9edcc4d90ce3e1f2a057c7b825b02e6c0492a43a
olli <olli.seiffermann@outlook.de>
2019-01-20 16:01:00 +0100

    made functions and attributes semi-private

--- hidden_canvas.py
+++ hidden_canvas.py
@@ -4,9 +4,9 @@ from tkinter import Canvas
 class HiddenCanvas(Canvas):
     def __init__(self, window):
         super().__init__(window, width=4200, height=4200)
-        self.rectangle = self.create_rectangle(20, 20, 4180, 4180, fill="blue", width=0)
-        self.circle = self.create_oval(20, 20, 4180, 4180, fill="red", width=0)
+        self._rectangle = self.create_rectangle(20, 20, 4180, 4180, fill="blue", width=0)
+        self._circle = self.create_oval(20, 20, 4180, 4180, fill="red", width=0)
 
-    def get_closest(self, x, y):
+    def _get_closest(self, x, y):
         return self.find_closest(x, y)
 
--- visible_canvas.py
+++ visible_canvas.py
@@ -3,13 +3,13 @@ from tkinter import Canvas
 
 class VisibleCanvas(Canvas):
     CIRCLE_COLOR = "#000000"
-    RECTANGLE_COLOR = "#FFFFFF"
-    BG_COLOR = "#161618"
+    RECTANGLE_COLOR = "#888888"
+    BG_COLOR = "#888888"
 
     def __init__(self, window):
-        super().__init__(window, width=420, height=420, borderwidth=0)
-        self.rectangle = self.create_rectangle(0, 0, 420, 420, fill=self.RECTANGLE_COLOR, width=0)
-        self.circle = self.create_oval(0, 0, 420, 420, fill=self.CIRCLE_COLOR, width=0)
+        super().__init__(window, width=420, height=420, borderwidth=0, bg=self.BG_COLOR)
+        self._rectangle = self.create_rectangle(0, 0, 420, 420, fill=self.RECTANGLE_COLOR, width=0)
+        self._circle = self.create_oval(2, 2, 420, 420, fill=self.CIRCLE_COLOR, width=0)
 
-    def draw_point(self, x, y, color):
+    def _draw_point(self, x, y, color):
         self.create_oval(x, y, x + 1, y + 1, width=0, fill=color)
